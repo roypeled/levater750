57ac1dea67142294e43c11cd2cb5aa77e7630a1a
Roy Peled <peled.roy@gmail.com>
2020-07-16 00:31:36 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let button = document.querySelector("#copyButton");
+button.addEventListener('click', copyText);
+
+addOrgs();
+
+function copyText() {
+  let textarea = document.createElement('textarea');
+  let quote = document.querySelector("blockquote");
+  let name = document.querySelector("#name");
+  let id = document.querySelector("#id");
+
+  let fullText = quote.innerText;
+  fullText = fullText
+      .replace(/##name##/, name.value)
+      .replace(/##id##/, id.value)
+      .replace(/\r?\n|\r/g, '');
+
+  textarea.value = fullText;
+
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  button.classList.add('success');
+  button.innerHTML = "הועתק, לחץ שוב כדי להעתיק"
+}
+
+function addOrgs() {
+  let orgsList = document.querySelector("#orgs");
+  orgs
+    .sort(() => Math.random() - .5)
+    .map(org => `<li><a href='${org.link}'>${org.name}</li>`)
+    .forEach(html => orgsList.innerHTML += html);
+}
+
+
---
+++ js/orgs.js
@@ -0,0 +1,8 @@
+let orgs = [
+  { link: "https://www.pitchonlev.org.il/", name: "פתחון לב" },
+  { link: "https://www.elem.org.il/", name: "על\"מ" },
+  { link: "https://igy.org.il/", name: 'איגי' },
+  { link: "https://www.latet.org.il/", name: 'לתת' },
+  { link: "https://yad-sarah.net/", name: 'יד שרה' },
+  { link: "https://www.ami.org.il/", name: 'עזר מציון' },
+];
